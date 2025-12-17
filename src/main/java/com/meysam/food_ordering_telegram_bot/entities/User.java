77afde73f958234c20a7e6556c23d0dd3ee78660
Sam-TN <meysamtaherinia76@gmail.com>
2025-12-17 18:36:25 +0330

    adds user entity

---
+++ src/main/java/com/meysam/food_ordering_telegram_bot/entities/User.java
@@ -0,0 +1,26 @@
+package com.meysam.food_ordering_telegram_bot.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @Column(name = "telegram_id")
+    private Long telegramId; // The unique ID from Telegram
+
+    private String username;
+    private String firstName;
+    private String lastName;
+
+}
