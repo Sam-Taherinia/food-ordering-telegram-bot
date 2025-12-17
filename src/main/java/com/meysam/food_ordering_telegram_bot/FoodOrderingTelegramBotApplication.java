f93de18840e29fd0c2732eb24d780deb18422fb8
Sam-TN <meysamtaherinia76@gmail.com>
2025-12-17 18:12:54 +0330

    Initial commit

---
+++ src/main/java/com/meysam/food_ordering_telegram_bot/FoodOrderingTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.meysam.food_ordering_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingTelegramBotApplication.class, args);
+	}
+
+}
