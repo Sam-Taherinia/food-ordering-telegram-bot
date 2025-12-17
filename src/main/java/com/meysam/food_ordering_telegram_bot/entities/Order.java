43257a1b205ce84b35527480bd6e3d3914101867
Sam-TN <meysamtaherinia76@gmail.com>
2025-12-17 18:46:19 +0330

    adds order entity

---
+++ src/main/java/com/meysam/food_ordering_telegram_bot/entities/Order.java
@@ -0,0 +1,29 @@
+package com.meysam.food_ordering_telegram_bot.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    private Integer quantity;
+
+}
